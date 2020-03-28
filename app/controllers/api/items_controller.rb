e0a88239a9ef8358785e84c31b7d955fe0d21634
alberto <a.mmazaira@gmail.com>
2020-03-28 13:41:43 +0100

    Add tags to Api create

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -4,8 +4,8 @@ class Api::ItemsController < ApplicationController
 
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    @item = Item.new(link: item_params[:link], user_id: item_params[:user_id])
+    if current_user.tag(@item, with: item_params[:tag_list], on: :tags)
       render json: @item
     else
       render json: { error: '422, something went wrong' }
@@ -21,7 +21,12 @@ class Api::ItemsController < ApplicationController
       @item = Item.find(params[:id])
     end
 
+    # TODO: remove user_id as soon as we have auth
     def item_params
-      params.require(:item).permit(:link, :user_id, :tag_list)
+      params.require(:item).permit(:link, :user_id, tag_list: [])
+    end
+
+    def current_user
+      User.first
     end
 end
