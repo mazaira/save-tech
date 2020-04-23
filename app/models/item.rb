6a314cb8715d1fbf1ed5a5f3df023f51081017eb
alberto <a.mmazaira@gmail.com>
2020-04-23 15:28:00 +0200

    Edit adding tags

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,17 @@ class Item < ApplicationRecord
 
   acts_as_taggable
 
-  def tag_list
-    tags.join(", ")
-  end
-
   def meta
     resp = Faraday.get(ENV['META_URL'], {url: self.link}, {'Authorization' => ENV['META_KEY']})
 
     JSON.parse(resp.body)['meta']
+  end
+
+  def add_tag(tags)
+    self.user.tag(self, with: tags, on: :tags)
+  end
+
+  def tag_list
+    tags.join(", ")
   end
 end
