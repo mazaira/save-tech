2f589db2e769e1a7629e2f827769169950791f5e
alberto <a.mmazaira@gmail.com>
2020-05-13 19:42:15 +0200

    Scroll to top

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -40,3 +40,19 @@ $(document).ready(() => {
     });
   });
 });
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  var mybutton = document.getElementById("goTop");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+window.topFunction = function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+}
