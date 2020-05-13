43c1a915dc5efabd16281a5735f30ce6615a7075
alberto <a.mmazaira@gmail.com>
2020-05-13 16:21:29 +0200

    Return 6 tags

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -5,7 +5,7 @@ class Api::TagsController < Api::BaseController
   end
 
   def most_used
-    tags = current_user.owned_tags.order('taggings_count DESC').first(5).pluck(:name)
+    tags = current_user.owned_tags.order('taggings_count DESC').first(6).pluck(:name)
     render json: { data: { type: 'tags', attributes: tags}}
   end
 end
