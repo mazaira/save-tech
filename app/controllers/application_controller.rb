d50614ccde5fe06a63e0c8ec75fff42fd8cdaa48
alberto <a.mmazaira@gmail.com>
2020-05-14 11:27:38 +0200

    Add fuck you to json requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
-  private
+  rescue_from ActionController::UnknownFormat, with: :render_fuck_you
+
+    private
+  def render_fuck_you
+    render json: { hey: "you are trying to break the app. Just don't do it!"}
+  end
 
   def set_raven_context
     Raven.user_context(id: session[:current_user_id]) # or anything else in session
